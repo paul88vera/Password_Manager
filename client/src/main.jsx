ccad6aad154c8691c4f2e8bc91d720ac52d4abdd
paul88vera <paul88vera@gmail.com>
2025-09-18 09:06:12 -0500

    before helmet added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,7 @@ import { ErrorBoundary } from "./pages/ErrorBoundary.jsx";
 import "./index.css";
 import { ClientRoute } from "./pages/Client.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { HelmetProvider } from "react-helmet-async";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -18,7 +19,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <ErrorBoundary fallback={ClientRoute}>
-        <RouterProvider router={router} />
+        <HelmetProvider>
+          <RouterProvider router={router} />
+        </HelmetProvider>
       </ErrorBoundary>
     </ClerkProvider>
   </StrictMode>
